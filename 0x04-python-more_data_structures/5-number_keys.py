85242b5519de35098cf08ac8ad10e16cddfdef56
Kossy-Augusta <kossyudogu@gmail.com>
2023-12-20 02:14:24 -0800

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+    Function
+    number_keys - Function that returns the number of keys in a dictionary.
+
+    Arguments
+    @a_dictionary: a dictionary of key and value pairs
+'''
+
+
+def number_keys(a_dictionary):
+    return (len(a_dictionary))
