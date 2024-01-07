8010d6e4bc4e9043db9c6ad66e05aac4548ec8a2
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-07 06:04:26 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             num += 1
-        print()
         return num
     except IndexError:
         return num
+    finally:
+        if num > 0:
+            print()
