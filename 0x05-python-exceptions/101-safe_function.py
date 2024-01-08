6cef743d68b0797d8264e776b777953463442ca8
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-08 02:20:03 -0800

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,4 +15,4 @@ def safe_function(fct, *args):
         return(fct(*args))
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
-        return None
+        return (None)
