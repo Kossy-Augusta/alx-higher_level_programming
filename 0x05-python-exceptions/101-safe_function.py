03697d7afd1a615671da8ca0ebc0f57fe5fa0dcc
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-08 02:18:01 -0800

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+'''
+safe_function - function that executes a function safely.
+@fct: pointer to a function
+@args: Variadic number odf arguments
+Return: result of the function fct otherwise return None if an exception \
+        occurs during the function and prints in stderr the error\
+        precede by Exception:
+'''
+
+
+def safe_function(fct, *args):
+    try:
+        return(fct(*args))
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
