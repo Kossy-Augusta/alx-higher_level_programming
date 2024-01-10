1520759b19a53021b6d1c068909a2200e8d1fcaf
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-10 11:55:05 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""0-squar Module
+
+
+This module has just one attribute which is the Square class
+"""
+
+
+class Square:
+    """Square class
+
+
+    defines a simple square
+    """
+    pass
