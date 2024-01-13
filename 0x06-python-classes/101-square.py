ee137f207f667d2d416d071aa09076c69dd40073
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-13 06:34:40 -0800

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -79,7 +79,7 @@ class Square:
         """Print the square with character #"""
         if self.__size == 0:
             print()
-            return
+            return("")
 
         [print() for space in range(self.__position[1])]
         for i in range(self.__size):
@@ -88,14 +88,12 @@ class Square:
             print()
 
     def __str__(self):
-        """Prints the output just as my_print"""
-        result = ""
-        if self.__size == 0:
-            result += "\n"
-            return result
-        result += "\n" * self.__position[1]
-        for i in range(self.__size):
-            result += " " * self.__position[0]
-            result += "#" * self.__size
-            result += "\n"
-        return result
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
